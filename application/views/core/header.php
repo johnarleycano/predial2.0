0a81bad9108b07367e9f78497ae65d045096ed00
John Arley <johnarleycano@hotmail.com>
2018-04-17 11:42:47 -0500

    feat(general): Instalación de Materialize CSS

--- application/views/core/header.php
+++ application/views/core/header.php
@@ -5,4 +5,20 @@
 <!-- Título que viene desde el controlador de cada interfaz -->
 <title><?php echo $titulo; ?> | SIstema Predial</title>
 
-Cabecera
+<?php if(ENVIRONMENT === 'development') { ?>
+	<!-- Estilos -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/materialize.css" />
+	
+
+	<!-- Scripts -->
+	<script src="<?php echo base_url(); ?>js/materialize.js"></script> <!-- Scripts para UI Kit -->
+<?php } ?>
+
+<?php if(ENVIRONMENT === 'production') { ?>
+	<!-- Estilos -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/materialize.min.css" />
+	
+
+	<!-- Scripts -->
+	<script src="<?php echo base_url(); ?>js/materialize.min.js"></script> <!-- Scripts para UI Kit -->
+<?php } ?>

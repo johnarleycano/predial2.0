b1623630e714add5636ec87d90977b75da954d04
John Arley <johnarleycano@hotmail.com>
2018-04-17 11:56:53 -0500

    refactor(general): renovación de fuentes

--- application/views/core/header.php
+++ application/views/core/header.php
@@ -3,13 +3,11 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 <!-- Título que viene desde el controlador de cada interfaz -->
-<title><?php echo $titulo; ?> | SIstema Predial</title>
+<title><?php echo $titulo; ?> | Sistema Predial</title>
 
 <?php if(ENVIRONMENT === 'development') { ?>
 	<!-- Estilos -->
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/materialize.css" />
-	<link rel="stylesheet" href="<?php echo base_url(); ?>css/estilos.min.css" />
-	
 
 	<!-- Scripts -->
 	<script src="<?php echo base_url(); ?>js/materialize.js"></script>
@@ -17,11 +15,12 @@
 
 <?php if(ENVIRONMENT === 'production') { ?>
 	<!-- Estilos -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>css/materialize.min.css" />
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway" rel="stylesheet">
 
 	<!-- Scripts -->
 	<script src="<?php echo base_url(); ?>js/materialize.min.js"></script>
 <?php } ?>
 
-<!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> -->
+<!-- Fuentes -->
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/materialize.min.css" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/estilos.min.css" />
